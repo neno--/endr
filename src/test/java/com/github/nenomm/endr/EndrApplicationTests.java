4bb30d3d9d831d067212e58c96bb4a6d2d659685
nenad <nenad.nenadovic@kapsch.net>
2018-03-14 22:24:25 +0100

    Initialize Spring Boot 2 Add .editorconfig Add .gitignore

---
+++ src/test/java/com/github/nenomm/endr/EndrApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.nenomm.endr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EndrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
